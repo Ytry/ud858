b3a39ffcb420b11afeea4fb92a6ef6075106fbd6
Ytry <Kyle12qw@gmail.com>
2015-11-14 05:24:31 -0500

    sessionKeysToAttend
    Added a list of session keys to be used as a users session whitelist as
    well store a sessions websafe key

--- ConferenceCentral_Complete/models.py
+++ ConferenceCentral_Complete/models.py
@@ -29,6 +29,7 @@ class Profile(ndb.Model):
     mainEmail = ndb.StringProperty()
     teeShirtSize = ndb.StringProperty(default='NOT_SPECIFIED')
     conferenceKeysToAttend = ndb.StringProperty(repeated=True)
+    sessionKeysToAttend = ndb.StringProperty(repeated=True)
 
 
 class ProfileMiniForm(messages.Message):
@@ -43,6 +44,8 @@ class ProfileForm(messages.Message):
     mainEmail = messages.StringField(2)
     teeShirtSize = messages.EnumField('TeeShirtSize', 3)
     conferenceKeysToAttend = messages.StringField(4, repeated=True)
+    sessionKeysToAttend = messages.StringField(5, repeated=True)
+    websafeSessionKey = messages.StringField(6, repeated=True)
 
 
 class StringMessage(messages.Message):
